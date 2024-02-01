4987b9543001e3194e39f9f8b76fe96bfc1774ee
anshuul <anshultated10@gmail.com>
2024-02-02 00:32:26 +0530

    commit

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,15 +1,15 @@
 export const API_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
     ? import.meta.env.VITE_BACKEND_SERVER + 'api/'
-    : 'http://localhost:8888/api/';
+    : 'https://bot2do-crm-ci1g.onrender.com/api/';
 export const BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER
-    : 'http://localhost:8888/';
+    : 'https://bot2do-crm-ci1g.onrender.com/';
 export const DOWNLOAD_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER + 'download/'
-    : 'http://localhost:8888/download/';
+    : 'https://bot2do-crm-ci1g.onrender.com/';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
 
 //  console.log(
